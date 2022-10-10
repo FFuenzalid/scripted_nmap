3bc200352e63c2e711cea9ddd34ff5903bbb2eb2
Francisco Fuenzalida <f.fuenzalida.91@gmail.com>
2022-10-10 04:13:28 +0000

    docstring for execute_nmap

--- main.py
+++ main.py
@@ -5,7 +5,30 @@ import os
 from datetime import datetime
 
 def execute_nmap(source_file, output_file):
-    # ONLY VALID FOR UNIX
+    """
+    Takes a file that contains ip's or network segments separated by newlines
+    and returns the raw result of nmap execution of the command explicitly declared below
+
+    Parameters
+    ----------
+    source_file : string
+        Absolute reference to the file.
+        Example: /home/user/file.txt
+    output_file : string
+        Absolute reference to the file where the output will be stored
+    
+    Returns
+    -------
+        On Unix:
+            Status Code of the execution, 0 is ok
+        On Windows:
+            Output of the Terminal
+
+    Examples
+    --------
+    >>> execute_nmap(home/user/input.txt, home/user/out/output.txt)
+    0
+    """
     base, filename = os.path.split(output_file)
     print(base)
     assert os.path.isfile(source_file), f"The InputFile: {source_file}, Does Not Exist"
@@ -15,7 +38,7 @@ def execute_nmap(source_file, output_file):
     nmap_exec_code = os.system(command) # For Windows it will be the output of the command
     return nmap_exec_code
 
-def main() -> int:
+def main():
     now = datetime.now()
     pwd = os.getcwd()
     scan_date = now.strftime("%Y-%m-%d")
