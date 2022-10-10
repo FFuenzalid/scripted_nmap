540e88cb52791c85ddc149e667281fc82ddfe405
Francisco Fuenzalida <f.fuenzalida.91@gmail.com>
2022-10-10 03:55:07 +0000

    hotfix 10102022

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import time
 import os 
 from datetime import datetime
 
-def execute_nmap(source_file, output_file) -> int:
+def execute_nmap(source_file, output_file):
     # ONLY VALID FOR UNIX
-    (base, filename) = os.path.basename(output_file)
+    base, filename = os.path.split(output_file)
+    print(base)
     assert os.path.isfile(source_file), f"The InputFile: {source_file}, Does Not Exist"
     assert os.path.isdir(base), f"The Path: {base}, Doesn't Exist."
 
     command = f"nmap -sV -T4 -A -v --script vulners.nse -iL {source_file} -oN {output_file}"
-    nmap_exec_code = os.system(command)
+    nmap_exec_code = os.system(command) # For Windows it will be the output of the command
     return nmap_exec_code
 
 def main() -> int:
